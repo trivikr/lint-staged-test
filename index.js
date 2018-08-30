fa8f9566f45f9bd5c50d184c223bbe41e9678d8d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-08-30 05:25:36 -0700

    Rename req, res to request, response

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 // Test commit to ensure lint-staged runs prettier on git commit
-const server = http.createServer((req, res) => res.end("Hello World"));
+const server = http.createServer((request, response) =>
+  response.end("Hello World")
+);
 
 // A comment to separate two hunks
-server.listen(3000);
+server.listen(5000);
